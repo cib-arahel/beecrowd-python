62051eae741e66600a710dd8cbd457a0d79a6a45
Cibele Arahel <99376314+cib-arahel@users.noreply.github.com>
2022-08-18 19:18:20 -0400

    beecrowd | 1113

---
+++ bee-1113.py
@@ -0,0 +1,20 @@
+#crescente e decrescente
+
+X, Y = input().split(" ")
+X = int(X)
+Y = int (Y)
+
+if X < Y:
+    if X < Y:
+       print("Crescente")
+    elif x > Y:
+        print("Decrescente")
+
+while X != Y:
+    X, Y = input().split(" ")
+    X = int(X)
+    Y = int (Y)
+    if X < Y:
+        print("Crescente")
+    elif X > Y:
+        print("Decrescente")
