f4e23439026ce448a678ff8cd075a3ff05a40187
Cibele Arahel <99376314+cib-arahel@users.noreply.github.com>
2022-08-18 19:01:59 -0400

    beecrowd | 1052

---
+++ bee-1052.py
@@ -0,0 +1,19 @@
+#mês
+
+meses = list(range(13))
+meses[1] = 'January'
+meses[2] = 'February'
+meses[3] = 'March'
+meses[4] = 'April'
+meses[5] = 'May'
+meses[6] = 'June'
+meses[7] = 'July'
+meses[8] = 'August'
+meses[9] = 'September'
+meses[10] = 'October'
+meses[11] = 'November'
+meses[12] = 'December'
+
+
+x = int(input())
+print (meses[x])
